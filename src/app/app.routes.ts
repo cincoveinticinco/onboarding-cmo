ca8f2c8c85ddb94d5c2f56347d22a46d5b14b139
carreraprogrammer <carreraprogrammer@gmail.com>
2024-05-31 14:46:22 -0500

    feat: Add routes for home, form, and page not found components
    chore: Update assets configuration in angular.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FormsCmoComponent } from './pages/forms-cmo/forms-cmo.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+			path: 'home',
+			component: HomeComponent
+    },
+    {
+			path: 'form',
+			component: FormsCmoComponent,
+    },
+		{
+			path: '**',
+			component: PageNotFoundComponent
+		}
+];
