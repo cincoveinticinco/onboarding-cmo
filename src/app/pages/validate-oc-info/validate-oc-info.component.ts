60338f99fde166f2bad4aa7189d01000e08e1fa4
Daniel <Daniel.Carrera@nbcuni.com>
2024-08-05 17:55:20 -0500

    feat: Add validate-oc-info component and styles

---
+++ src/app/pages/validate-oc-info/validate-oc-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validate-oc-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './validate-oc-info.component.html',
+  styleUrl: './validate-oc-info.component.css'
+})
+export class ValidateOcInfoComponent {
+
+}
