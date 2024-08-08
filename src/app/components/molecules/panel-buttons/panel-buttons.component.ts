6298c2731190689e489e3f85eee2d2eedd8b3863
Daniel <Daniel.Carrera@nbcuni.com>
2024-08-08 11:01:52 -0500

    feat: Add AuthOcService for handling authentication and session management

--- src/app/components/molecules/panel-buttons/panel-buttons.component.ts
+++ src/app/components/molecules/panel-buttons/panel-buttons.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output, input } from '@angular/core';
 import { BlackButtonComponent } from '../../atoms/black-button/black-button.component';
 import { AuthService } from '../../../services/auth.service';
 import { VendorService } from '../../../services/vendor.service';
+import { AuthOcService } from '../../../services/auth-oc.service';
 
 @Component({
   selector: 'app-panel-buttons',
@@ -14,8 +15,13 @@ import { VendorService } from '../../../services/vendor.service';
 })
 export class PanelButtonsComponent {
   @Output() saveForm: EventEmitter<any> = new EventEmitter();
-  constructor(private auth: AuthService, private _vS: VendorService) {}
+  @Input() isOc: boolean = false;
+  constructor(private auth: AuthService, private _vS: VendorService, private authOc: AuthOcService) {}
   logOut() {
-    this.auth.logOut(this._vS.getVendorId());
+    if (!this.isOc) {
+      this.auth.logOut(this._vS.getVendorId());
+    } else {
+      this.authOc.logOut();
+    }
   }
 }
