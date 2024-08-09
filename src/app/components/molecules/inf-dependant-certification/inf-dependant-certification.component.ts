1c6f4e1b3cb1cd931b2a019b6d90930812095ce9
Daniel <Daniel.Carrera@nbcuni.com>
2024-08-09 17:36:08 -0500

    feat: Update inf-dependant-certification component with form fields and dynamic rendering

--- src/app/components/molecules/inf-dependant-certification/inf-dependant-certification.component.ts
+++ src/app/components/molecules/inf-dependant-certification/inf-dependant-certification.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { SubtitleComponent } from '../../atoms/subtitle/subtitle.component';
+import { TextInputComponent } from '../../atoms/text-input/text-input.component';
+import { SelectInputComponent } from '../../atoms/select-input/select-input.component';
+import { CheckboxInputComponent } from '../../atoms/checkbox-input/checkbox-input.component';
+import { FileboxComponent } from '../../atoms/filebox/filebox.component';
 
 @Component({
   selector: 'app-inf-dependant-certification',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    SubtitleComponent,
+    TextInputComponent,
+    SelectInputComponent,
+    CheckboxInputComponent,
+    FileboxComponent
+  ],
   templateUrl: './inf-dependant-certification.component.html',
   styleUrl: './inf-dependant-certification.component.css'
 })
 export class InfDependantCertificationComponent {
+  @Input() dependantForm: any;
+  @Input() dependentIndex: number = 0;
 
+  getControl(controlName: string) {
+    return this.dependantForm?.get(controlName);
+  }
+
+  getValue(controlName: string) {
+    return this.dependantForm?.get(controlName)?.value;
+  }
 }
