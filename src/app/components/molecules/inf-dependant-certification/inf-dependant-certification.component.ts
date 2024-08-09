25339dc5a7699730254abb49c3fa5788411615fa
Daniel <Daniel.Carrera@nbcuni.com>
2024-08-09 15:06:04 -0500

    feat: Add inf-dependant-certification component

---
+++ src/app/components/molecules/inf-dependant-certification/inf-dependant-certification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inf-dependant-certification',
+  standalone: true,
+  imports: [],
+  templateUrl: './inf-dependant-certification.component.html',
+  styleUrl: './inf-dependant-certification.component.css'
+})
+export class InfDependantCertificationComponent {
+
+}
