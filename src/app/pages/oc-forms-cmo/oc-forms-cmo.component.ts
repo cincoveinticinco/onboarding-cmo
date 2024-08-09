aa87baff389fdd4b477fe3a6978ee6794d0313ab
Daniel <Daniel.Carrera@nbcuni.com>
2024-08-09 15:04:44 -0500

    style: Update form container styles in oc-forms-cmo component

--- src/app/pages/oc-forms-cmo/oc-forms-cmo.component.ts
+++ src/app/pages/oc-forms-cmo/oc-forms-cmo.component.ts
@@ -1,26 +1,54 @@
 import { Component } from '@angular/core';
 import { PanelButtonsComponent } from '../../components/molecules/panel-buttons/panel-buttons.component';
 import { AuthOcService } from '../../services/auth-oc.service';
+import { InvoiceLodgingService } from '../../services/invoiceLodging.service';
+import { Router } from '@angular/router';
+import { LogoComponent } from '../../components/atoms/logo/logo.component';
+import { InvoiceNaturalFormComponent } from '../../components/organisms/invoice-natural-form/invoice-natural-form.component';
 
 @Component({
   selector: 'app-oc-forms-cmo',
   standalone: true,
-  imports: [PanelButtonsComponent],
+  imports: [PanelButtonsComponent, LogoComponent, InvoiceNaturalFormComponent],
   templateUrl: './oc-forms-cmo.component.html',
   styleUrl: './oc-forms-cmo.component.css'
 })
 export class OcFormsCmoComponent {
-  constructor( private auth: AuthOcService ) {
+  loading: boolean = false;
+  vendorInfo: any = {};
+  currentStep = 1;
 
+  constructor( 
+    private auth: AuthOcService, 
+    private _iS: InvoiceLodgingService, 
+    private router: Router,
+  ) {
+    
   }
 
   ngOnInit() {
     this.auth.getSession().then((isLoggedIn: any) => {
       if (!isLoggedIn) {
         this.auth.logOut();
+      } else {
+        this.loadFormInitialData();
       }
     });
   }
+
+  loadFormInitialData() {
+    this.loading = true;
+    this._iS.getFormInitialData().subscribe(
+      (response: any) => {
+        this.vendorInfo = response.vendor;
+        this.loading = false;
+      },
+      (error) => {
+        this.auth.logOut();
+      } 
+    );
+  }
+
   saveForm() {
     console.log('Form saved');
   }
