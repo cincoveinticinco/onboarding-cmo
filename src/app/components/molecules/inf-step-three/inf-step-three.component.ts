54c321221b6b7edb7ccf15a2e1922de37580d308
Daniel <Daniel.Carrera@nbcuni.com>
2024-08-09 15:05:38 -0500

    feat: Add inf-step-three component

---
+++ src/app/components/molecules/inf-step-three/inf-step-three.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inf-step-three',
+  standalone: true,
+  imports: [],
+  templateUrl: './inf-step-three.component.html',
+  styleUrl: './inf-step-three.component.css'
+})
+export class InfStepThreeComponent {
+
+}
